9c84d69847650a071d69a63960165912f5d19c9d
brunoj <bruno.jamula@gmail.com>
2023-04-07 10:05:02 +0200

    update qwik to 0.101

--- src/root.tsx
+++ src/root.tsx
@@ -1,9 +1,6 @@
 import { component$ } from "@builder.io/qwik";
-import {
-  QwikCity,
-  RouterOutlet,
-  ServiceWorkerRegister,
-} from "@builder.io/qwik-city";
+import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from '@builder.io/qwik-city';
+
 import { RouterHead } from "./components/router-head/router-head";
 
 import "./global.css";
@@ -16,7 +13,7 @@ export default component$(() => {
    * Dont remove the `<head>` and `<body>` elements.
    */
   return (
-    <QwikCity>
+    <QwikCityProvider>
       <head>
         <meta charSet="utf-8" />
         <RouterHead />
@@ -25,6 +22,6 @@ export default component$(() => {
         <RouterOutlet />
         <ServiceWorkerRegister />
       </body>
-    </QwikCity>
+    </QwikCityProvider>
   );
 });
